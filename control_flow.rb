82096d8e0d13ee1ddf8bea486d090a87160f17c5
Yontida Yosbut <a.yontida@gmail.com>
2021-12-18 16:22:12 -0500

    commit tree 1fa60904106a9180628f1895ee8e5e1f0fb047ef

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,61 @@
+#1
 def admin_login(username, password)
   # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
 
+
+#2
 def hows_the_weather(temperature)
   # your code here
+  weather =  if temperature < 40
+                "brisk"
+              elsif temperature >= 40 && temperature <= 65
+                "a little chilly"
+              elsif temperature > 85
+                "too dang hot"
+              else
+                "perfect"
+              end
+
+    "It's #{weather} out there!"
 end
 
+
+#3
 def fizzbuzz(num)
   # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else 
+    num
+  end
 end
 
+
+#4
 def calculator(operation, num1, num2)
   # your code here
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+    nil
+  end
 end
 
